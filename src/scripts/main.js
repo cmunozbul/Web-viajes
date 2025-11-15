cefb7e7942b44feff3dac633003a396869e3c8e9
Cristina Muñoz <cmunozbul@uoc.edu>
2025-11-15 16:05:55 +0100

    Funcionalidad de menú activo Javascript

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const currentPath = window.location.pathname.split("/").pop(); 
+    let activePage;
+
+    if (currentPath === "") {
+        activePage = "index.html";
+    } else {
+        activePage = currentPath;
+    }
+
+    const navLinks = document.querySelectorAll('header nav ul li a');
+
+    navLinks.forEach(link => {
+        const linkPath = link.getAttribute('href').split("/").pop();
+
+        if (linkPath === activePage) {
+            link.classList.add('current');
+        }
+    });
+});
